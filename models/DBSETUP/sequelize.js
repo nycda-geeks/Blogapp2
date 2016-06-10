856a2900e86f6ef7d01740aba8c69b27bcc5ae35
Maartje Snoeck Henkemans <Maartje@Maartjes-MacBook-Pro.local>
2016-06-10 16:01:43 +0200

    ehrmegerd echt done

--- models/DBSETUP/sequelize.js
+++ models/DBSETUP/sequelize.js
@@ -2,6 +2,6 @@
 var express = require('express')
 var path = require('path')
 var Sequelize = require('sequelize')
-var sequelize = new Sequelize('postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + ':hartje123@192.168.99.100:32768/blogapp2')
-
+// var sequelize = new Sequelize('postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + ':hartje123@192.168.99.100:32768/blogapp2')
+var sequelize = new Sequelize('postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/blogapp2')
 module.exports = sequelize
